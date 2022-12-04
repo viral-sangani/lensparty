c0b7ef7a22f0e98aca35741c05c4319b34f20b0d
Viral Sangani <viral.sangani2011@gmail.com>
2022-12-04 07:10:17 +0530

    Bug fixes

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
     'unused-imports/no-unused-imports': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'no-console': 'off',
-    'react-hooks/exhaustive-deps': 'off'
+    'react-hooks/exhaustive-deps': 'off',
+    'prefer-destructuring': 'off',
+    'react/jsx-no-useless-fragment': 'off'
   }
 };
